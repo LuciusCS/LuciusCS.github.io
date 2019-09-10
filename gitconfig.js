ad7de4a6a222376debe5b07c905304e0eddf2da2
LuciusCS <LuciusZhao.cs@gmail.com>
2019-09-10 14:53:35 +0800

    init

---
+++ gitconfig.js
@@ -0,0 +1,10 @@
+var gitalk = new Gitalk({
+  "clientID": "5095be7a4e45ca487159",
+  "clientSecret": "31bef093f7940634657ae90fade6fd930e4cf608",
+  "repo": "GitBook",
+  "owner": "LuciusCS",
+  "admin": ["LuciusCS"],
+  "id": location.pathname,      
+  "distractionFreeMode": false  
+});
+gitalk.render("gitalk-container");
