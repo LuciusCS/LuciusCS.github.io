ca209f6113826e01f53e93e07e958b127eaef3ae
LuciusCS <LuciusZhao.cs@gmail.com>
2019-09-11 19:49:59 +0800

    Updates Config/gitalk-config.js
    Auto commit by GitBook Editor

--- Config/gitalk-config.js
+++ Config/gitalk-config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var gitalk = new Gitalk({  
   "clientID": "f405db0bc334c654cb02",  
   "clientSecret": "f1658ea7679789bb0b8f4c38bd46264eade59a9b",  
@@ -8,15 +8,4 @@ var gitalk = new Gitalk({
   "id": location.pathname,        
   "distractionFreeMode": false    
 });  
-=======
-var gitalk = new Gitalk({  
-  "clientID": "f405db0bc334c654cb02",  
-  "clientSecret": "f1658ea7679789bb0b8f4c38bd46264eade59a9b",  
-  "repo": "LuciusCS.github.io",  
-  "owner": "LuciusCS",  
-  "admin": ["LuciusCS"],  
-  "id": location.pathname,        
-  "distractionFreeMode": false    
-});  
->>>>>>> EditBranch
 gitalk.render("gitalk-container");  
