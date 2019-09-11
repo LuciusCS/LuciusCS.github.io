2118a4c7bbf0cf0e8be2aed47d02d2787aa65cf8
LuciusCS <LuciusZhao.cs@gmail.com>
2019-09-11 15:58:07 +0800

    merge

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var gitalk = new Gitalk({  
   "clientID": "f405db0bc334c654cb02",  
   "clientSecret": "f1658ea7679789bb0b8f4c38bd46264eade59a9b",  
@@ -7,4 +8,15 @@ var gitalk = new Gitalk({
   "id": location.pathname,        
   "distractionFreeMode": false    
 });  
+=======
+var gitalk = new Gitalk({  
+  "clientID": "f405db0bc334c654cb02",  
+  "clientSecret": "f1658ea7679789bb0b8f4c38bd46264eade59a9b",  
+  "repo": "LuciusCS.github.io",  
+  "owner": "LuciusCS",  
+  "admin": ["LuciusCS"],  
+  "id": location.pathname,        
+  "distractionFreeMode": false    
+});  
+>>>>>>> 0535f9b... add gitalk-config
 gitalk.render("gitalk-container");  
