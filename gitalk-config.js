50ab34d6a3e0d2e02d7c7f05ff9f060608134b1c
LuciusCS <LuciusZhao.cs@gmail.com>
2019-09-11 16:03:49 +0800

    # This is a combination of 2 commits. # This is the 1st commit message:
    merge
    
    # This is the commit message #2:
    
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    merge
    
    # This is the commit message #2:
    
    add gitalk-config

---
+++ gitalk-config.js
@@ -0,0 +1,10 @@
+var gitalk = new Gitalk({  
+  "clientID": "f405db0bc334c654cb02",  
+  "clientSecret": "f1658ea7679789bb0b8f4c38bd46264eade59a9b",  
+  "repo": "LuciusCS.github.io",  
+  "owner": "LuciusCS",  
+  "admin": ["LuciusCS"],  
+  "id": location.pathname,        
+  "distractionFreeMode": false    
+});  
+gitalk.render("gitalk-container");  
